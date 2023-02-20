ae196fa407cc40d2e1fd06464d2b331ba7927ece
ajayiw <ajayiw@coventry.ac.uk>
2023-02-20 12:26:28 +0000

    Testing main push

--- main.cpp
+++ main.cpp
@@ -2,3 +2,44 @@
 // this is a test for changes
 //pull test
 
+
+#include <iostream>
+#include <string>
+#include "Pokemon.h"
+#include "Pokemon_moves.h"
+using namespace std;
+
+class Player
+{
+    public:
+        int poke_Balls = 0;
+        int pokemon_Count = 0;
+        string player_Name = "";
+};
+
+int main()
+{
+    //declare objects in main to be able to use their functions
+    Player player;
+    NPC Pokemon1;
+    NPC Pokemon2;
+   
+    
+
+
+
+
+
+    cout << endl;
+
+   
+   
+    
+ 
+
+
+    
+
+
+    return 0;
+}
