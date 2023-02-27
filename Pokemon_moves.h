e8714035012b80b4e63077fc0fd7d12affe93c99
ajayiw <ajayiw@coventry.ac.uk>
2023-02-27 17:35:58 +0000

    Ife's update on the pokemon moves (works)

--- Pokemon_moves.h
+++ Pokemon_moves.h
@@ -1,6 +1,35 @@
 #include <iostream>
 #include <string>
 #include <array>
+#include <cstdlib>
+#include <random>
+#include <algorithm>
 using namespace std;
 
-//test for pull
+class Electric
+{
+public:
+    string move_name;
+    int damage;
+    
+    //creates the class
+    Electric()
+    {
+        cout<<endl;
+    }
+
+    void electric_Move1()
+    {
+        move_name = "Zippy zap";
+        damage = 50;
+    }
+
+
+    void print_Status()
+    {   
+        cout << "This move is called : " << move_name << endl;
+        cout << "And it does " << damage << " damage." << endl;
+
+    }
+    
+};   
