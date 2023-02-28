8fe1f430f6b1e984f9330db193c1b22a56aaced0
Mmesoma Nwachukwu (nwachukwum) <nwachukwum@coventry.ac.uk>
2023-02-28 10:10:22 +0000

    Update Pokemon_moves.h

--- Pokemon_moves.h
+++ Pokemon_moves.h
@@ -33,3 +33,36 @@ public:
     }
     
 };   
+
+#include <iostream>
+
+using namespace std;
+class Moves{//A Moves superclass that the different types should inherit from. 
+protected:// protected attributes so only the child classes can have access to them
+    string pokemon;
+    string move_name;
+    string type;
+    int damage;//Below are the Moves initialiser and constructor respectively. 
+    Moves(){
+        pokemon = "no pokemon";
+        move_name = "no move";
+        type = "no type";
+        damage = 0;
+    }
+    Moves(string aPokemon, string aMove_name, string aType, int aDamage){
+        pokemon = aPokemon;
+        move_name = aMove_name;
+        type = aType;
+        damage = aDamage;
+    }
+};
+class Water : public Moves{// Water class being a child class to the Moves class
+public:
+    Water(string aPokemon, string aMove_name, int aDamage): Moves(aPokemon, aMove_name, "Water", aDamage){};
+};
+
+void printPokemonMoves(const Moves& poke)//take a const Moves& reference instead of a Moves object to avoid unnecessary copying, and prints pokemon moves. 
+{
+    cout<<poke.pokemon<<"uses"<<poke.move_name<<"that causes"<<poke.damage<<"damage"<<endl;
+}
+
