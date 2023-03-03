398cc07de8100653b0355570a0adf56ff428b749
Mmesoma Nwachukwu (nwachukwum) <nwachukwum@coventry.ac.uk>
2023-03-03 21:33:59 +0000

    Update Pokemon_moves.h

--- Pokemon_moves.h
+++ Pokemon_moves.h
@@ -151,3 +151,100 @@ string get_Move_Name()
     }
 
 };
+class Water {
+public:
+    string move_name;
+    int damage;
+
+    Water() {
+        move_name = "name";
+        damage = 0;
+    }
+
+    // Getters
+    int get_Damage() {
+        return damage;
+    }
+
+    string get_Move_Name() {
+        return move_name;
+    }
+
+    void water_Move1() {
+        move_name = "Water gun";
+        damage = 40;
+    }
+
+    void water_Move2() {
+        move_name = "Surf";
+        damage = 70;
+    }
+
+    void water_Move3() {
+        move_name = "Hydro pump";
+        damage = 90;
+    }
+};
+
+class Grass {
+public:
+    string move_name;
+    int damage;
+
+    Grass() {
+        move_name = "name";
+        damage = 0;
+    }
+
+    // Getters
+    int get_Damage() {
+        return damage;
+    }
+
+    string get_Move_Name() {
+        return move_name;
+    }
+
+    void grass_Move1() {
+        move_name = "Vine whip";
+        damage = 45;
+    }
+
+    void grass_Move2() {
+        move_name = "Razor leaf";
+        damage = 70;
+    }
+
+    void grass_Move3() {
+        move_name = "Solar beam";
+        damage = 90;
+    }
+};
+
+class Fighting {
+public:
+    string move_name;
+    int damage;
+
+    Fighting() {
+        move_name = "name";
+        damage = 0;
+    }
+
+    //
+
+void fightingMove1() {
+        move_name = "Karate chop";
+        damage = 50;
+    }
+
+    void fightingMove2() {
+        move_name = "Brick break";
+        damage = 75;
+    }
+
+    void fightingMove3() {
+        move_name = "Superpower";
+        damage = 120;
+    }
+};
