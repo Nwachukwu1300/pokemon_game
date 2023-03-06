19cf322743d14a883be22bb72512e59aa91bb53f
Ife1770 <ifeadu10@gmail.com>
2023-03-06 00:53:24 +0000

    updated moves getters, added if statements to determine pokemon types and separated pokemons by type using vectors

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <random>
 #include "Pokemon.h"
-#include "Pokemon_moves.h"
+
 #include "battles.h"
 using namespace std;
 
@@ -20,39 +20,57 @@ int main()
 
     //random generator to create a random number
     mt19937 rng(time(0)); // https://stackoverflow.com/questions/57135552/how-can-i-set-range-to-the-random-library
-    uniform_int_distribution d(0, 8); // The range of the length of pokemon file (hardcoded must be updated if more pokemon are added)
+    uniform_int_distribution <int> d(0, 8); // The range of the length of pokemon file (hardcoded must be updated if more pokemon are added)
     
     //declare objects in main to be able to use their functions
     Player player;
-    Electric electric_Move1;
-    Normal normal_Move1;
+    Dragon dragon;
     
     // the random generator, generating a random number
     int randomInt = d(rng);
     NPC Pokemon1;   //creating the first object
     Pokemon1.pick_Random_Pokemon(randomInt); //calling the method to picka random pokemon to be assigned to the object
-    Pokemon1.print_stats(); //just a test to see if works (will delete soon)
+    Pokemon1.print_stats();
+    Pokemon1.pokemon_type(); //just a test to see if works (will delete soon)
 
     randomInt = d(rng);
     NPC Pokemon2;
     Pokemon2.pick_Random_Pokemon(randomInt);
     Pokemon2.print_stats();
+    Pokemon2.pokemon_type();
 
     randomInt = d(rng);
     NPC Pokemon3;
     Pokemon3.pick_Random_Pokemon(randomInt);
     Pokemon3.print_stats();
+    Pokemon3.pokemon_type();
 
     randomInt = d(rng);
     NPC Pokemon4;
     Pokemon4.pick_Random_Pokemon(randomInt);
     Pokemon4.print_stats();
+    Pokemon4.pokemon_type();
 
     randomInt = d(rng);
     NPC Pokemon5;
     Pokemon5.pick_Random_Pokemon(randomInt);
     Pokemon5.print_stats();
+    Pokemon5.pokemon_type();
 
+    cout << endl;
+
+//all works if ife uses his code on pokemon_moves
+    string move;
+    int damage;
+
+    move = dragon.get_move2_name();
+
+    cout << endl;
+
+    damage = dragon.get_move1_damage();
+
+    cout << "The move is " << move << endl;
+    cout << damage;
 
     cout << endl;
 
@@ -60,11 +78,10 @@ int main()
     int new_Hp = 0;
 
     //checking getters are working 
-    normal_Move1.normal_Move1(); //to make the function work for the moves, we must call it first like this
-    cout << Pokemon1.get_Name() << " has been hit with "<< normal_Move1.get_Move_Name() << endl << Pokemon1.get_Name() << " had " << Pokemon1.get_Hp() << "HP." << endl;
+    cout << Pokemon1.get_Name() << " has been hit with zippy zap!" <<endl << Pokemon1.get_Name() << " had " << Pokemon1.get_Hp() << "HP." << endl;
 
     //method to calculate the new hp of the pokemon inputtting the orginal hp and the damage the move does (hard coded the damnage normally will have to fetch it from the pokemon moves from whatever move the user choses but the code is not finished yet so i used 50 for now)
-    new_Hp = attack(Pokemon1.get_Hp(),normal_Move1.get_Damage());
+    new_Hp = attack(Pokemon1.get_Hp(),50);
 
     //set the new hp of the pokemon 
     Pokemon1.set_Hp(new_Hp);
@@ -76,5 +93,6 @@ int main()
     Pokemon1.print_stats();
    
     cout << endl;
+
     return 0;
 }
