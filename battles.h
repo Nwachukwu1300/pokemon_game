0bc96dfaebdf9e4e2f7d290160c6b796eb687885
Mmesoma Nwachukwu (nwachukwum) <nwachukwum@coventry.ac.uk>
2023-03-08 11:12:03 +0000

    Update battles.h

--- battles.h
+++ battles.h
@@ -23,3 +23,32 @@ int attack(int pokemon_Hp, int damage_Done)
     //return the new hp after the attack has been done
     return new_Hp;
 }
+
+
+/* A FUNCTION THAT GETS HOW EFFECTIVE A MOVE TYPE IS AGAINST ANOTHER POKEMON TYPE. 
+double calculate_effectiveness(string move_type, string defender_type) {
+    double effectiveness = 1.0;
+    if(move_type == "water") {
+        if(defender_type == "grass" {
+            effectiveness = 2.0;
+        } else if(defender_type == "water") || defender_type == "normal"|| defender_type == "fighting"{
+            effectiveness = 0.5;
+        } else if(defender_type == "dragon")|| defender_type == "electric" {
+            effectiveness = 0.25;
+        }
+    } else if(move_type == "grass") {
+        // similar logic as for water type
+    } else if(move_type == "normal") {
+        // logic for normal type
+    } else if(move_type == "fighting") {
+        // logic for fighting type
+    } else if(move_type == "electric") {
+        // logic for electric type
+    } else if(move_type == "dragon") {
+        // logic for dragon type
+    } else {
+        // handle invalid move type
+    }
+    return effectiveness;
+}
+*/
